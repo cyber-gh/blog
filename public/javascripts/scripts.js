51176f3c591daf2726b1dee7758c9bb9771dbc32
cyber-gh <solgheorghe@gmail.com>
2019-10-27 01:59:04 +0300

    ajax calls fully integrated

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -3,7 +3,7 @@ function isDeleteButton(btn) {
 }
 
 let listenerByIndex = function(index) {
-    return function curried_func(e) {
+    return function curried_func() {
         let toBeDeleted = document.getElementsByTagName("article")[index];
         toBeDeleted.parentNode.removeChild(toBeDeleted);
         setupListeners();
@@ -15,11 +15,6 @@ function setupNavBarListeners() {
     refresBtn.addEventListener("click",
         function () {
             getArticlesAsync();
-        });
-
-    document.getElementById("add-article").addEventListener("click",
-        function () {
-            addNewArticle("test", "test", "test");
         });
 
 }
@@ -42,12 +37,24 @@ let articlesEndpoint = "http://localhost:3000/api/v1/articles";
 function getArticlesAsync() {
     var xhhtp = new XMLHttpRequest();
     xhhtp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            alert("Request successfull " + this.responseText);
+        if (this.readyState === 4 && this.status === 200) {
+            populateArticles(JSON.parse( this.responseText)["articles"] );
+
         }
-    }
+    };
     xhhtp.open("GET", articlesEndpoint);
     xhhtp.send();
+}
+
+function populateArticles(data) {
+    document.getElementById("articles-container").innerHTML = '';
+
+    for (var i = 0; i < data.length; i++) {
+        const el = data[i];
+        console.log(el);
+        addNewArticle(el.title, el.date, el.text);
+    }
+    setupListeners();
 }
 
 window.addEventListener("load", function () {
@@ -62,9 +69,11 @@ function addNewArticle(title, date, text) {
     var articleTitle = articleClone.querySelector("h1.article-title");
     articleTitle.textContent = title;
     articleClone.querySelector("div.article-date").textContent = date;
-    articleClone.querySelector("p.article-date").textContent = text;
+    articleClone.querySelector("p.article-text").textContent = text;
 
 
     document.getElementById("articles-container").appendChild(articleClone);
     setupListeners();
 }
+
+getArticlesAsync();
